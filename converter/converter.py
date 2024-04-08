b376977f028a19e4a9bb52bfeaad2ca9d78d4e2c
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2024-04-08 16:09:05 +0500

    Add check negative value

--- converter/converter.py
+++ converter/converter.py
@@ -1,5 +1,5 @@
 class Converter:
     def convert(self, from_value, to_value, amount):
-        if (from_value==0):
-            raise ValueError("From value must not be zero.")
+        if (from_value<=0):
+            raise ValueError("From value must not be zero and not negative.")
         return to_value/from_value*float(amount)
