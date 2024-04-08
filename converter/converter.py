90af801a0284954f1ce3f32af4010fdf664ea12e
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2024-04-08 16:53:40 +0500

    Remove some changes

--- converter/converter.py
+++ converter/converter.py
@@ -2,4 +2,4 @@ class Converter:
     def convert(self, from_value, to_value, amount):
         if (from_value<=0):
             raise ValueError("From value must not be zero and not negative.")
-        return to_value/from_value*float(amount)*1
+        return to_value/from_value*float(amount)
